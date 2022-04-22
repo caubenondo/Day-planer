64b57161fa259863c2927c4903dbc57d5ee3a2fb
Hai Duong <HaiDuong@MEA110MACPRO-04.local>
2022-04-22 14:20:26 -0700

    done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,12 @@
 const currentDay = $('#currentDay');
+// default array of planner
 let planner = [
-    [9,''],[10,''],[11,''],[12,''],[13,''],[14,''],[15,''],[16,''],[17,'']
+    [9,''],[10,''],[11,'test'],[12,''],[13,''],[14,''],[15,''],[16,''],[17,'']
 ];
-let currentHour = '';
+let currentHour = 0;
 let checkHour = setInterval(function () {
-    if(moment().format('h')!=currentHour){
-        currentHour = moment().format('h');
+    if(moment().hours()!=currentHour){
+        currentHour = moment().hours();
         updateTable();
     }
 }, 1000);
@@ -13,22 +14,31 @@ let checkHour = setInterval(function () {
 function main(){    
     // display current day
     currentDay.text(moment().format('dddd, MMMM Do'));
-    currentHour = moment().format('h');
-    $('.testing').text(moment().format('h'));
-    const hour = moment().format('h');
-    console.log(hour);
+    currentHour = moment().hours();
     // get local planer
-    console.log(currentHour);
+    // console.log(currentHour);
     getLocalPlanner();
     updateTable();
-    
 }
 
+$('#plannerDisplay').on('click','.btn', function(e){
+    // const element = e.target;
+    // console.log(element.closest('tr'));
+    storePlanner();
+});
+
+
+
 function getLocalPlanner(){
-    planner = JSON.parse(localStorage.getItem('Planer')) || planner;
+    // if there is no array in local storage
+    planner = JSON.parse(localStorage.getItem('Planner')) || planner;
 }
 function storePlanner(){
-    localStorage.setItem('Planer',localStorage.setItem('Planer',JSON.stringify(planner)));
+    for (let i = 9;i<=17;i++){
+        planner[i-9][1] = $(`#input-${i}`).val().trim();
+    }
+    // console.log(planner);
+    localStorage.setItem('Planner',JSON.stringify(planner));
 }
 
 function updateTable(){
@@ -38,27 +48,15 @@ function updateTable(){
         htmlTemplate += `
         <tr data-hour="${row[0]}">
             <td class="text-end"><h3>${row[0]<=12? row[0]:row[0]%12}:00 ${row[0]<12?'AM':'PM'}</h3></td>
-        <td><textarea class="form-control ${row[0]<parseInt(currentHour)?'bg-secondary':(row[0]==parseInt(currentHour)? 'bg-primary':'')}" 
-            id="input-${row[0]}" rows="2">${row[1]}
-            </textarea>
-        <td><button class="btn btn-success">Save</button></td>
+        <td>
+            <textarea placeholder="To do" class="form-control ${row[0]<currentHour?'bg-secondary text-white':row[0]==currentHour? 'bg-danger text-white':'bg-success text-white'}" 
+                id="input-${row[0]}" rows="2">${row[1].trim()}</textarea>
+        <td><button type="button"  class="btn btn-info fa-2x"><i class="fas text-white fa-font-awesome fa-save"></i></button></td>
         </tr>        
         `;
     }
+
     $('#plannerDisplay').html(htmlTemplate);
 }
 
 main();
-
-
-
-
-/* 
-<tr data-hour="10">
-    <td class="text-end">
-              <h3>12:00</h3>
-    </td>
-    <td><textarea class="form-control" id="exampleFormControlTextarea1" rows="2"></textarea>
-    <td><button class="btn btn-success">edit</button></td>
-</tr> 
-*/
