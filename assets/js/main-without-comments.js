a06d52be3effe5304aacf8c3b4f690b1a1315b43
Hai Duong <HaiDuong@MEA110MACPRO-04.local>
2022-04-22 15:34:51 -0700

    formated

--- assets/js/main-without-comments.js
+++ assets/js/main-without-comments.js
@@ -1,5 +1,4 @@
 const currentDay = $('#currentDay');
-// default array of planner
 let planner = [
     [9, ''],
     [10, ''],
@@ -11,10 +10,8 @@ let planner = [
     [16, ''],
     [17, '']
 ];
-// current hour tracker
 let currentHour = 0;
-// re-render the whole view if the current hour is change
-// this detect the moment when 59th turns to  00 minute  
+
 let checkHour = setInterval(function () {
     if (moment().hours() != currentHour) {
         currentHour = moment().hours();
@@ -22,38 +19,25 @@ let checkHour = setInterval(function () {
     }
 }, 1000);
 
-
-// store the planner everytime button click
-// take advantage of jquery event delegation feature, listen to button click only 
 $('#plannerDisplay').on('click', '.btn', function (e) {
-    // const element = e.target;
-    // console.log(element.closest('tr'));
     storePlanner();
 });
 
-// get the planner from local storage,
-// if there is no planner in local storage, return default planner which has empty inputs
+
 function getLocalPlanner() {
-    // if there is no array in local storage
     planner = JSON.parse(localStorage.getItem('Planner')) || planner;
 }
 
-// store the planner to local storage
 function storePlanner() {
-    // grab values of all textareas
     for (let hour of planner) {
         hour[1] = $(`#input-${hour[0]}`).val().trim();
     }
-    // console.log(planner);
     localStorage.setItem('Planner', JSON.stringify(planner));
 }
 
-// update/render the planner view  
 function updateTable() {
     let htmlTemplate = ``
-    // take advantage of ternary to do choose the right bg class for textarea
     for (let row of planner) {
-
         htmlTemplate += `
         <tr data-hour="${row[0]}">
             <td class="text-end" style="width: 15%"><p class='h3'>${row[0]<=12? row[0]:row[0]%12}:00 ${row[0]<12?'AM':'PM'}</p></td>
@@ -67,20 +51,13 @@ function updateTable() {
         </tr>        
         `;
     }
-
     $('#plannerDisplay').html(htmlTemplate);
 }
 
 function main() {
-    // display current day
     currentDay.text(moment().format('dddd, MMMM Do'));
-    // grab the current hour
     currentHour = moment().hours();
-    // get local planer
-    // console.log(currentHour);
-    // grab the planner from local storage
     getLocalPlanner();
-    // display the planner view
     updateTable();
 }
 main();
